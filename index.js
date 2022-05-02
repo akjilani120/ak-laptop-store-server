e9366985685daf53be57914d1421a94bf6827067
akjilani120 <abdulkaderjilani120@gmail.com>
2022-05-03 00:58:57 +0600

    mongodb server side added

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const cors = require('cors');
+
 const app = express()
 const port = process.env.PORT || 5000;
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://laptopStore:<password>@cluster0.emy94.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("laptopStore").collection("laptopProduct");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 // middle ware
 app.use(cors())

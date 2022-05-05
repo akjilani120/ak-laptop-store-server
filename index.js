1a29b6d409c9ae2bff4eb074b4d2819a8ea255e8
akjilani120 <abdulkaderjilani120@gmail.com>
2022-05-06 01:49:32 +0600

    api delete added

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const res = require('express/lib/response');
 const app = express()
 const port = process.env.PORT || 5000;
- require('dotenv').config()
-
+require('dotenv').config()
+app.use(cors())
+app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.emy94.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 const collection = client.db("laptopStore").collection("laptopProduct");
-async function run () {
-  try{
+async function run() {
+  try {
     await client.connect();
-    app.get('/products', async (req , res) =>{
-      const query ={};
+    app.get('/products', async (req, res) => {
+      const query = {};
       const cursor = collection.find(query)
       const result = await cursor.toArray();
       res.send(result)
     })
-    app.get('/products/:id', async (req, res) =>{
-      const id= req.params.id;
-      const query ={_id: ObjectId(id)}
+    app.get('/products/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) }
       const result = await collection.findOne(query);
+      res.send(result)
+    })
+    app.put('/products/:id', async (req, res) => {
+      const id = req.params.id
+      const updateQuantity = req.body
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: updateQuantity
+      };
+      const result = await collection.updateOne(filter, updateDoc, options);
+      res.send(result)
+    })
+    app.delete('/products/:id', async(req , res) =>{
+      const id = req.params.id
+      const query = { _id: ObjectId(id)}
+      const result = await collection.deleteOne(query);
       res.send(result)
     })
   }finally{
@@ -30,12 +49,11 @@ async function run () {
 run().catch(console.dir)
 
 // middle ware
-app.use(cors())
-app.use(express.json())
 
-app.get('/', (req , res) =>{
-    res.send("It is Laptop store shop")
+
+app.get('/', (req, res) => {
+  res.send("It is Laptop store shop")
 })
-app.listen(port, () =>{
-    console.log("I am ak laptop store shop", port)
+app.listen(port, () => {
+  console.log("I am ak laptop store shop", port)
 })
